e6317eb716d41f5205282585621798724c1cbca9
Anshul Bharat <anshul.ab17x@gmail.com>
2026-01-06 13:39:12 +0530

    Event_loop

--- src/app/server.rs
+++ src/app/server.rs
@@ -1,10 +1,43 @@
-use mio::Poll;
+use mio::{Events, Interest, Poll, Token};
+use mio::net::TcpListener;
+use std::io;
+use std::net::SocketAddr;
+
+const SERVER: Token = Token(0);
 
 pub struct Server;
 
 impl Server {
-    pub fn start(){
-        let _poll =Poll::new().expect("Poll failed");
-        print!("server booting!")
+    pub fn start() -> io::Result<()> {
+        let mut poll = Poll::new()?;
+        let mut events = Events::with_capacity(128);
+
+        let addr: SocketAddr = "127.0.0.1:9000".parse().unwrap();
+        let mut listener = TcpListener::bind(addr)?;
+
+        poll.registry().register(
+            &mut listener,
+            SERVER,
+            Interest::READABLE,
+        )?;
+
+        loop {
+            poll.poll(&mut events, None)?;
+
+            for event in events.iter() {
+                match event.token() {
+                    SERVER => {
+                        loop {
+                            match listener.accept() {
+                                Ok((_socket, _addr)) => {}
+                                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => break,
+                                Err(e) => return Err(e),
+                            }
+                        }
+                    }
+                    _ => unreachable!(),
+                }
+            }
+        }
     }
 }
